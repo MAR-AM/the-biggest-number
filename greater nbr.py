dc06329f1f88c7b6d2789e2799eeb18233f5e19d
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-10-27 23:44:47 +0100

    (⓿_⓿)

---
+++ greater nbr.py
@@ -0,0 +1,10 @@
+A=float(input('Enter the first number : '))
+B=float(input('Enter the second number : '))
+C=float(input('Enter the third number : '))
+Max=A
+if B>Max :
+  Max=B
+elif C>Max:
+  Max=C
+print('the biggest number is :',Max)
+
